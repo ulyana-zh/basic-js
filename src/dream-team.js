db4f9b618edb4f91db41e0503ab0c83863b4791c
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2020-10-15 18:45:03 +0400

    add solutions for count-cats, dream-team, what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(array) {
+  //throw new CustomError('Not implemented');
+  if (array == undefined || array.length === 0) return false;
+  let teamName = '';
+  for(i=0; i<array.length; i++) {
+    if(typeof array[i] === 'string') {
+      array[i] = array[i].trim();
+      teamName += array[i][0];
+  } 
+} return teamName.toUpperCase().split('').sort().join('');
+}
