14e17d881556297ff9bee98e73b15c479110c9ef
Ulyana Zhivotenkova <ulyanazh2@gmail.com>
2020-10-16 00:28:21 +0400

    feat: add solution for transform-array

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(diskNumber, turnsSpeed) {
+  //throw new CustomError('Not implemented');
+  let turnsNumber = Math.pow(2, diskNumber) - 1;
+  let secondsNumber = Math.floor(turnsNumber / (turnsSpeed / 3600));
+  let result = new Object();
+  return result = {
+    turns: turnsNumber,
+    seconds: secondsNumber
+  };
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  //throw new CustomError('Not implemented');
+
+  if (!Array.isArray(arr)) {
+		throw new Error('input argument must be an array')
+	}
+
+	let transformedArray = []
+
+	let doubleNext = false
+	let discardNext = false
+	let prevHasBeenDiscarded = false
+
+	for (let x of arr) {
+		switch (x) {
+			case '--discard-next':
+				discardNext = true
+				break;
+			case '--double-next':
+				doubleNext = true
+				break;
+			case '--discard-prev':
+				if (transformedArray.length > 0 && !prevHasBeenDiscarded) {
+					transformedArray.pop()	
+					prevHasBeenDiscarded = false
+				}
+				break;
+			case '--double-prev':
+				if (transformedArray.length > 0 && !prevHasBeenDiscarded) {
+					transformedArray.push(transformedArray[transformedArray.length - 1])
+					prevHasBeenDiscarded = false
+				}
+				break;
+			default:
+				if (discardNext) {
+					discardNext = false
+					prevHasBeenDiscarded = true
+					break;
+				}
+
+				prevHasBeenDiscarded = false;
+				transformedArray.push(x)
+				if (doubleNext) {
+					doubleNext = false
+					transformedArray.push(x)
+				}
+		}
+	}
+
+	return transformedArray;
+}
